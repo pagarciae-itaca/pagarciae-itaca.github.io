9bf2893bf8ddd68f528aeb0db4e73e9c59fbc291
pagarciae-itaca <57485122+pagarciae-itaca@users.noreply.github.com>
2019-11-29 13:19:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-pagarciae-itaca.github.io
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project: Recipe Book</title>
+        <meta charset="utf-8">
+        <style>
+            .encabezado_principal{background-color: #dddddd; color:firebrick;}
+            .encabezado_receta{background-color: #888888;}
+            #texto_remarcado{color: #8A8E27; font-style:italic;}
+        </style>
+    </head>
+    <body>
+       
+        <h1 class="encabezado_principal" id="a_principio">PABLO's web page</h1>
+        
+        <h2>Contents:</h2>
+        
+        <ol>
+            <li><a href="#tortilla">Página de viajes.</a></li>
+            <li><a href="#espaguetis">Mi blog.</a></li>
+            <li><a href="#serranito">Libro de recetas.</a></li>
+        </ol>
+        <p><a href="#a_principio">Volver al principio</a></p>
+    </body>
+</html>
